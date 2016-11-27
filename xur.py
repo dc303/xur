4acf8542d6e0ada8972c8a767d59e0db8d6a9f3e
DC <dc@technoexile.com>
2016-11-26 21:41:06 -0700

    env vars

--- xur.py
+++ xur.py
@@ -1,43 +1,40 @@
 # where is xur, what's he got, and where is trials?
 
-#from lxml import html
-#import json
-
 import requests
 import os,sys,traceback
 import sendgrid
 from sendgrid.helpers.mail import *
 
-# Uncomment this line to print JSON output to a file:
-# f = open('output.txt', 'w')
+bungo_apiKey=os.environ.get('BUNGIE_API_KEY')
+bungo_authHeader = {'X-API-Key': bungo_apiKey}
+sg_apiKey = os.environ.get('SENDGRID_API_KEY')
+emailTarget = "ninja-chemists@googlegroups.com"
+emailSender = "xur@ninjachemists.com"
+emailSubject = "TGIF: Update from the Nine"
 
-apiKey=os.environ['BUNGIE_API_KEY']
-BUNGO_AUTHHEADER = {'X-API-Key': apiKey}
-
-base_url = "https://www.bungie.net/platform/Destiny/"
-#xur_url = "https://www.bungie.net/Platform/Destiny/Advisors/Xur/"
 
 bungie_base_url = "https://www.bungie.net/Platform/Destiny/"
 xur_url = bungie_base_url + "Advisors/Xur/"
-
+hashType = "6"
 whereIsXur_url = "https://tellmewhereisxur.com/api"
 whereIsTrials_url = "http://api.destinytrialsreport.com/currentMap/"
-hashType = "6"
 
+DEBUG = 0
 
 def sendMail():
     try:
-        sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-        from_email = Email("guardian@ninjachemists.com")
-        subject = "TGIF: Xur and Trials update"
-        to_email = Email("ninja-chemists@googlegroups.com")
-        content = Content("text/plain", "Hello, Guardian!\n\nTrials this week is on " + whereIsTrials() + "\n\nXur is " + whereIsXur() + "\n\n\nHere's what Xur has this week:\n\n\n\n" + getXurInventory())
+        sg = sendgrid.SendGridAPIClient(apikey=sg_apiKey)
+        from_email = Email(emailSender)
+        subject = emailSubject
+        to_email = Email(emailTarget)
+        content = Content("text/plain", "Hello, Guardian!\n\nTrials this week is on " + whereIsTrials() + "\n\nI'm hiding out " + whereIsXur() + "\n\n\nHere's what I've got for you this week:\n\n\n\n" + getXurInventory() + "\n\nSorry this email is so ugly. The Nine won't let me use HTMl and inline CSS\n\n")
         mail = Mail(from_email, subject, to_email, content)
         response = sg.client.mail.send.post(request_body=mail.get())
-        print content.value
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        if DEBUG == 1:
+            print content.value
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
     except Exception:
         traceback.print_exc(file=sys.stdout)
         sys.exit(1)
@@ -56,8 +53,6 @@ def whereIsTrials():
 def whereIsXur():
     try:
         res = requests.get(whereIsXur_url)
-        #print "finding xur by hitting" + whereIsXur_url
-        #print "Xur is " + res.json()['location']
         return(res.json()['location'])
 
     except requests.exceptions.RequestException as e:
@@ -68,7 +63,8 @@ def whereIsXur():
 def getXurInventory():
     try:
         # Send the request and store the result in res:
-        res = requests.get(xur_url, headers=BUNGO_AUTHHEADER)
+        res = requests.get(xur_url, headers=bungo_authHeader)
+        #todo: return a list instead of a string when we're ready to send the email as HTML
         #xurStuff = []
         xurStuff = ""
         for saleItem in res.json()['Response']['data']['saleItemCategories']:
@@ -76,7 +72,7 @@ def getXurInventory():
             for myItem in mysaleItems:
                 hashID = str(myItem['item']['itemHash'])
                 hashReqString = bungie_base_url + "Manifest/" + hashType + "/" + hashID
-                res = requests.get(hashReqString, headers=BUNGO_AUTHHEADER)
+                res = requests.get(hashReqString, headers=bungo_authHeader)
                 item_name = res.json()['Response']['data']['inventoryItem']['itemName']
                 #print "Item is: " + item_name
                 item_type = res.json()['Response']['data']['inventoryItem']['itemTypeName']
@@ -100,21 +96,12 @@ def getXurInventory():
 def main():
     try:
         sendMail()
-        sys.exit(0)
-        #mailContent = ""
-        #print "Trials this week is on " + whereIsTrials()
-        #print "Xur is " + whereIsXur()
-        #print "\nHere's what Xur has this week:\n"
-        #for element in getXurInventory():
-        #    print '<td>%s</td>' % element
-        #print getXurInventory().items()
 
     except KeyboardInterrupt:
         print "Shutdown requested...exiting"
     except Exception:
         traceback.print_exc(file=sys.stdout)
     sys.exit(0)
-
 
 
 if __name__ == "__main__":
